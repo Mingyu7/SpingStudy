c76031882d1098dd82d952a93c6aa286d5e48ed9
Mingyu7 <120396128+Mingyu7@users.noreply.github.com>
2023-03-17 02:23:57 +0900

    DB 연결해서 sql 사용해 저장하기
    H2 연결해서 jdbc이용해서 저장 하기

--- SpringConfig.java
+++ SpringConfig.java
@@ -1,19 +1,30 @@
 package hellomin.hellomin.Service;
 
+import hellomin.hellomin.repository.JdbcMemberRepository;
 import hellomin.hellomin.repository.MemberRepository;
 import hellomin.hellomin.repository.MemoryMemberRepositroy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
 
-    @Bean //자바 코드로 스프링 빈에 등록
-    public MemberService memberService(){
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
+
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepositroy();
+    public MemberRepository memberRepository() {
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource); // 데이터 베이스에 저장
     }
+
 }
