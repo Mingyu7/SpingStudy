87c3180553cd29f9eb2e950f8da3b78a6edeada3
Mingyu7 <120396128+Mingyu7@users.noreply.github.com>
2023-03-23 15:41:42 +0900

    JpaMemberRepositoy
    스프링 설정
    spring.jpa.show-sql=true  jpa 가 자동으로 sql 만들어줌
    spring.jpa.hibernate.ddl-auto=none  jpa가 자동으로 테이블 만들어준다 하지만 none하면 X

---
+++ JpaMemberRepository.java
@@ -0,0 +1,33 @@
+package hellomin.hellomin.repository;
+
+import hellomin.hellomin.Domain.Member;
+
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository {
+    private final EntityManager em; // jpa 는 EntityManager가 관리한다
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+    public Optional<Member> findId(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+    public Optional<Member> findName(String name) {
+        List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return result.stream().findAny();
+    }
+}
