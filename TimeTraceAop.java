d246674325b97193501c2fb69d94a89fe11a695b
Mingyu7 <120396128+Mingyu7@users.noreply.github.com>
2023-03-25 16:01:56 +0900

    Aop
    Aop 적용 방법

---
+++ TimeTraceAop.java
@@ -0,0 +1,23 @@
+package hellomin.hellomin.Aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component // 스프링 적용
+@Aspect
+public class TimeTraceAop { // Aop 하는 클래스
+    @Around("execution(* hellomin.hellomin..*(..))") // hellomin 하위 모두 적용
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
